4b17af52ad804b82f7f4591abdc9949ab1732500
Graciano Kevin <93309424+Graciano7@users.noreply.github.com>
2023-02-08 19:04:02 +0700

    Add files via upload

---
+++ 2_D_71210691.py
@@ -0,0 +1,7 @@
+import collections
+string=input('kalimat yang ingin dicari : ')
+string=input('kata yang ingin dicari : ')
+
+#mencari jumlah kata
+hitungKata(kata, kalimat)
+input=string('jumlah kata yang dicari : ')
